af3c460146a44cf536364b89afdaeb0f4e89fdb2
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-12-12 10:46:30 +0100

    update cv

--- src/pages/Cv/Cv.jsx
+++ src/pages/Cv/Cv.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import cv from "../../assets/images/cv.jpg";
+import cv from "../../assets/images/CV.jpg";
 import "./Cv.scss";
 //import IconoDescarga from "../../../src/assets/images/icon-house.svg";
 import IconoDescarga from "../../../src/assets/images/pdf.png";
@@ -9,7 +9,7 @@ const Cv = () => {
     //funcion para descargar pdf
     const onButtonClick = () => {
         // using Java Script method to get PDF file
-        fetch('Oscar Sánchez-Marín Baquero cv.pdf').then(response => {
+        fetch('Oscar Sánchez-Marín Baquero_CV.pdf').then(response => {
             response.blob().then(blob => {
                 // Creating new object of PDF file
                 const fileURL = window.URL.createObjectURL(blob);
